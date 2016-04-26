97656604cfe93677c9092a0b5dbb4aef1da8f8f1
wizardbab <epang0522@students.pcci.edu>
2016-04-25 19:19:26 -0500

    Added my design code to David's and added the Score class

---
+++ Yahtzee/Yahtzee/Score.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yahtzee
+{
+    class Score
+    {
+        public int CalculateThreeOfAKind(Dice[] dice)
+        {
+            int sum = 0;
+
+            bool isThreeOfAKind = false;
+
+            for (int i = 1; i <= 6; i++)
+            {
+                int count = 0;
+                for (int j = 0; j < 5; j++)
+                {
+                    if (dice[j] == i)
+                        count++;
+
+                    if (count > 2)
+                        isThreeOfAKind = true;
+                }
+            }
+
+            if (isThreeOfAKind)
+            {
+                for (int k = 0; k < 5; k++)
+                {
+                    sum += dice[k];
+                }
+            }
+
+            return sum;
+        }
+
+        public int CalculateFourOfAKind(Dice[] dice)
+        {
+            int sum = 0;
+
+            bool isFourOfAKind = false;
+
+            for (int i = 1; i <= 6; i++)
+            {
+                int count = 0;
+                for (int j = 0; j < 5; j++)
+                {
+                    if (dice[j] == i)
+                        count++;
+
+                    if (count > 3)
+                        isFourOfAKind = true;
+                }
+            }
+
+            if (isFourOfAKind)
+            {
+                for (int k = 0; k < 5; k++)
+                {
+                    sum += dice[k];
+                }
+            }
+
+            return sum;
+        }
+    }
+}
